21852b9074e9f25f0a70f2fd215c4fe4f688799b
Tems17 <funmibipopoola@gmail.com>
2025-09-30 00:20:23 -0400

    Add files via upload

---
+++ Assignment2/templates/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="description" content="Pizza Order">
+    <meta charset="utf-8">
+    <title>Pizza Order</title>
+    <link rel=stylesheet href="./css/style.css">
+</head>
+<body>
+    <header>
+           <h1 class="comp-name">Pizza Palace</h1>
+           <h2 class="motto">When in doubt, pizza it out!</h2>
+    <div class="form-container">
+        <img src="./img/pizza-img.jpg" alt="Pizza Image">
+</div>
+
+        <h2 class="free-delivery">Free Home Delivery</h2>
+        <h2 class="order">Order Online</h2>
+        <p class="contact-info">
+            <span>Phone: +1 (356) 724-4565</span>
+            <span>Website: www.pizzapalace.com</span>
+</p>
+
+    
+</header>
